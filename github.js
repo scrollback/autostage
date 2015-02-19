b78e207f0ae6f22cc19742ba7881c03bbcaddb1f
chandrakant <ja.chandrakant@gmail.com>
2015-02-19 17:35:14 +0530

    added function to make changes into a file

--- github.js
+++ github.js
@@ -1,34 +1,50 @@
-var fs = require('fs'),
-	config = require('./config');
+var fs = require('fs-extra'),
+    base_dir = '/home/scrollback/scrollback',
+    config = require('./config');
 
-module.exports.createDomain = function(user, branch) {
-	//console.log("Create domain:", arguments);
-	var teamMembers = config.teamMembers;
-	if (teamMembers.indexOf(user) > -1) {
-		fs.readFile(config.nginx_path+ "scrollback", function(err, data) {
-			if (err) throw err;
-			data = data.toString('utf-8');
-			var lines = data.split("\n");
-			var newdata = [];
-			lines.forEach(function(line) {
-				if (/server_name /.test(line)) {
-					line = line.replace("stage.scrollback.io", branch + ".scrollback.com");
-					//console.log(line);
-				}
-			
-				newdata.push(line);
-			});
-			// set up new scrollback
-			// change port on config.
-			// create new database and create tables.
-			// create new redis databases or edit server-config.js database numbers.
+module.exports.createDomain = function (user, branch) {
+    var teamMembers = config.teamMembers;
+    if (teamMembers.indexOf(user) > -1) {
 
-			data = newdata.join("\n");
-			console.log("new data is ", data);
-			fs.writeFile(config.nginx_path + branch + '.com', data, function(err) {
-				if (err) throw err;
-			});
-		});
-	}
+        changeFile(config.nginx_path + "scrollback", "/server_name /", "stage.scrollback.io",
+            branch + ".scrollback.com");
+
+        fs.copy(base_dir, base_dir '-' + branch, function (err) {
+            if (err) return console.error(err);
+        });
+
+        // change config.
+        changeFile(base_dir '-' + branch + 'server-config.js',
+            "/stage.scrollback.io/", "stage.scrollback.io", branch + ".scrollback.com");
+
+        changeFile(base_dir '-' + branch + 'client-config.js', "/host: /", "//stage.scrollback.io",
+            branch + ".scrollback.com");
+
+        // create new database and create tables.
+        // create new redis databases or edit server-config.js database numbers.
+
+    }
+};
+
+var changeFile = function (path, regex, old_line, new_line) {
+    fs.readFile(path, function (err, data) {
+        if (err) throw err;
+        data = data.toString('utf-8');
+        var lines = data.split("\n");
+        var newdata = [];
+        lines.forEach(function (line) {
+            if (regex.test(line)) {
+                line = line.replace(old_line, new_line);
+                //console.log(line);
+            }
+            newdata.push(line);
+        });
+
+        data = newdata.join("\n");
+        console.log("new data is ", data);
+        fs.writeFile(path, data, function (err) {
+            if (err) throw err;
+        });
+    });
 };
 //createDomain("chandrascrollback", "helo");
