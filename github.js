29ced284dd1b10b7030032ed98e0bc40b47c42bd
Chandrakanta Shatapathy <ja.chandrakant@gmail.com>
2015-07-08 17:46:46 +0530

    fixed

--- github.js
+++ github.js
@@ -62,7 +62,7 @@ var deleteDomain = function(branch) { //delete the directory when a pull request
 			if (err) log.e(err);
 			log.i('Removing nginx config files');
 		});
-		childProcess.exec('sudo rm -rf /etc/init/ ' + branch + '.conf /var/run/scrollback-' + branch, function(err) {
+		childProcess.exec('sudo rm -rf /etc/init/' + branch + '.conf /var/run/scrollback-' + branch, function(err) {
 			if (err) log.e(err);
 			log.i('Removing upstart config files');
 		});
